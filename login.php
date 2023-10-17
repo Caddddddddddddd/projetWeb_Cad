8d5481eab3e18c2fd1f3d72ec98091e61388699f
kcap02 <kencapochichi@yahoo.com>
2023-10-18 00:31:48 +0200

    HomePage

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('base.html.php');
+?>
+
+  <!-- block title -->
+{% block title %}Hello AuthentificationController!{% endblock %}
+
+ <!-- block body -->
+
+<?php
+include('loginform.html');
+?>
+
+</html>
