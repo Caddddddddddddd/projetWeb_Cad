35a8b4acb54b4f0e4c5cb5b40b2c50b44720abb7
kcap02 <kencapochichi@yahoo.com>
2023-11-12 00:18:43 +0100

    HomePage&ConnexionAdmin

---
+++ connexion.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST['envoyer'])) {
+
+    $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
+    $username = $_POST['identifiant'];
+    $password = $_POST['motDePasse'];
+    echo $user['nom'];
+    
+    $req = $bdd->prepare("SELECT id, prenom, mdp,role FROM users WHERE email = ?");
+    $req->execute([$username]);
+    $user = $req->fetch();
+    echo $user['prenom'];
+
+    if ($user && $password==$user['mdp'] && $user['role'] == "membre" ) {
+       
+
+        // Redirigez l'utilisateur vers la page d'accueil des membres
+        header('Location: accueilMembre.php');
+        exit();
+    } //elseif ($user && password_verify($password, $user['mdp']) && $user['role'] == "administrateur" ){
+        //header('Location: acceuilAdmin.php');
+        //exit();
+    //}
+    else{
+        header('Location: loginform.php');
+        exit();
+    }
+}
+    ?>
--- loginform.php
+++ loginform.php
@@ -1,4 +1,4 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
@@ -7,15 +7,17 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,500&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>Document</title>
 </head>
+
 <body>
   
   <div class="PagedeConnection">
     <div class="enregistrement" id="formulaire">
       <h2 class="Titre_Formulaire">Formulaire</h2>
       <div class="form-formulaire-container">
-        <form action=""method="" class="form_formulaire">
+        <form action="connexion.php" method="post" class="form_formulaire">
           <div class="form_top">
             <div class="form_left">
               <div>
@@ -31,7 +33,7 @@
             </div>
           </div>
           <div class="form-bottom">
-            <input type="submit" name="Envoyer" value="Se connecter">
+            <input type="submit" name="envoyer" value="Se connecter">
 
           </div>
         </form>
