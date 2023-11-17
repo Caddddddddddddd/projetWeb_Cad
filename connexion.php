6d41fc9a1f7f08d2f59cfb22a82be25ca62e6e39
agossa <farelagossa@gmail.com>
2023-11-17 18:29:17 +0100

    Modification connexion

--- connexion.php
+++ connexion.php
@@ -1,30 +1,35 @@
+
 <?php
-//Debut 
-if (isset($_POST['envoyer'])) {
-
-    $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
-    $username = $_POST['identifiant'];
-    $password = $_POST['motDePasse'];
-    echo $user['nom'];
+    if (isset($_POST['envoyer'])) {
+        include('connection.php');
+        global $db;
+        $username = $_POST['identifiant'];
+        $password = $_POST['motDePasse'];
+        
     
-    $req = $bdd->prepare("SELECT id, prenom, mdp,role FROM users WHERE email = ?");
-    $req->execute([$username]);
-    $user = $req->fetch();
-    echo $user['prenom'];
-
-    if ($user && $password==$user['mdp'] && $user['role'] == "membre" ) {
+        $req = $db->prepare("SELECT id_membre, pseudo, email, passwords, privilege FROM tablemembre WHERE email = ?");
+        $req->execute([$username]);
+        $user = $req->fetch(); 
+    echo $user['pseudo'];
+//(password_verify($password,$row["password"])
+    if (password_verify($password,$user["passwords"])) {
        
- 
-        // Redirigez l'utilisateur vers la page d'accueil des membres
-        header('Location: accueilMembre.php');
+        if ($user['privilege'] == "admin") {
+            session_start();
+            $_SESSION['privilege'] == "admin";
+            header('Location: acceuilAdmin.php');
+            exit();
+        }elseif($user['privilege'] == "membre"){
+            session_start();
+            $_SESSION['privilege'] == "membre";
+            header('Location: accueilMembre.php');
         exit();
-    } //elseif ($user && password_verify($password, $user['mdp']) && $user['role'] == "administrateur" ){
-        //header('Location: acceuilAdmin.php');
-        //exit();
-    //}
+        }     
     else{
         header('Location: loginform.php');
         exit();
     }
+    }
 }
+        
     ?>
