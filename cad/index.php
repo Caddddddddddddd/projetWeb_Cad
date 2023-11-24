42ae880222a586f56effac4d25873a8ca44f0970
kcap02 <kencapochichi@yahoo.com>
2023-11-24 11:39:41 +0100

    Modification Page d'accueil1.1

--- cad/index.php
+++ cad/index.php
@@ -63,45 +63,57 @@ session_start();
 
     <div class="container" id="top-cards">
         <h1>Jeux Top 4</h1>
-        <div class="row">
-            <div class="col-md-5 py-3 py-md-0">
-                <div class="card" style="background-color: #1c1c50;">
-                    <img src="../images/Puissance 4.jpeg" alt="">
-                    <div class="card-img-overlay">
-                        
-                        <h5 class="card-titel">Puissance 4</h5>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae asperiores nisi sapiente cum maiores, harum consectetur a. Dolorem, nemo! Dolores mollitia laborum vel nihil sequi tempora aperiam quos alias non?</p>
-                    </div>
+        <div class="row ">
+            
+            <div class="col-md-5 py-3 py-md-0 mb-4">
+                
+                <div class="card" >
+                    <img src="../images/rami-multijoueur-html5-2021.gif" alt="">
+                    <h5 class="card-titel">RAMI</h5>
+                </div>
+            </div>
+            <div class="col-md-5 py-3 py-md-0 mb-4">
+               
+                <div class="card" >
+                    <img src="../images/tic-tac-toe-couv-788x444.png" alt="">
+                    <h5 class="card-titel">MORPION</h5>
                 </div>
             </div>
             <div class="col-md-5 py-3 py-md-0">
-                <div class="card" style="background-color: #1c1c50;">
-                    <img src="../images/Puissance 4.jpeg" alt="">
-                    <div class="card-img-overlay">
-                        
-                        <h5 class="card-titel">Puissance 4</h5>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae asperiores nisi sapiente cum maiores, harum consectetur a. Dolorem, nemo! Dolores mollitia laborum vel nihil sequi tempora aperiam quos alias non?</p>
-                    </div>
+                
+                <div class="card" >
+                    <img src="../images/183009455-design-plat-illustration-vectorielle-moderne-concept-de-stratégie-d-entreprise-avec-des-figures-d.jpg" alt="">  
+                    <h5 class="card-titel">ECHECS</h5>
                 </div>
             </div>
             <div class="col-md-5 py-3 py-md-0">
-                <div class="card" style="background-color: #1c1c50;">
-                    <img src="../images/Puissance 4.jpeg" alt="">
-                    <div class="card-img-overlay">
-                        
-                        <h5 class="card-titel">Puissance 4</h5>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae asperiores nisi sapiente cum maiores, harum consectetur a. Dolorem, nemo! Dolores mollitia laborum vel nihil sequi tempora aperiam quos alias non?</p>
-                    </div>
+                
+                <div class="card" >
+                    <img src="../images/6da8e6912a0063ccc7a37a8f0ec0f320_icon.png" alt="">  
+                    <h5 class="card-titel">YATZY</h5>
                 </div>
             </div>
         </div>
     </div>
+    
 
-    <div class="banner">
+    <div class="banner" id="a_propos">
         <div class="content">
-            <h1>Get Up To 50% off</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga saepe in mollitia incidunt blanditiis. Error similique praesentium vel, porro, doloremque consequuntur debitis alias corporis quod fuga, ratione nihil. Ipsum, ad?</p>
-            <div id="bannerbtn"><button>JOUER</button></div>
+            <h1>A propos</h1>
+            <p>PlateauVirtuoso est bien plus qu'un simple site de jeu de plateau en ligne. C'est une communauté florissante d'amateurs de jeux qui partagent une passion commune pour l'aventure, la stratégie, et l'amusement sans bornes.<br></p>
+<h1>Notre Mission</h1><br>
+
+<p>Chez PlateauVirtuoso, notre mission est de rapprocher les joueurs du monde entier à travers une expérience de jeu de plateau immersive et conviviale. Nous croyons au pouvoir des jeux pour connecter les gens, stimuler l'imagination, et créer des souvenirs durables.<br></p>
+<h1>Pourquoi Choisir PlateauVirtuoso ?</h1><br>
+
+<p>Diversité des Jeux : Parcourez notre vaste collection de jeux de plateau, des classiques intemporels aux dernières créations. Il y en a pour tous les goûts et tous les niveaux d'expérience.
+
+    Multijoueur en Ligne : Jouez avec vos amis, votre famille ou des adversaires du monde entier. PlateauVirtuoso offre une plateforme conviviale pour des défis stratégiques passionnants.
+
+    Expérience Immersive : Plongez dans des mondes fantastiques avec des graphismes captivants, des animations fluides, et des effets sonores immersifs. Laissez-vous transporter dans l'univers magique des jeux de plateau.
+
+    Communauté Engagée : Rejoignez une communauté dynamique de joueurs passionnés. Échangez des stratégies, partagez des astuces, et découvrez de nouveaux amis qui partagent votre amour pour les jeux de plateau.?</p>
+            
         </div>
     </div>
 
@@ -109,47 +121,29 @@ session_start();
     <div class="footer-top">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6 footer-links">
-                    <h3>Fasion</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum aperiam eligendi maiores neque commodi enim sequi nostrum dolor maxime, dicta incidunt fugit inventore soluta iste nesciunt libero odit necessitatibus?</p>
-                    <p>
-                        Karcahi<br><br>
-                        Sind<br><br>
-                        Pakistan<br><br>
-                    </p>
-                    <strong><i class="fas fa-phone"></i>Phone: <strong>+0000000000</strong></strong>
-                    <strong><i class="fas-solid fa-envelope"></i>Email: <strong>example@gmail.com</strong></strong>
+                <div class="col-lg-4 col-md-6 footer-links">
+                    <h3>PlateauVirtuoso</h3>
+                    <p> S'engage à offrir une expérience de jeu en ligne sécurisée, équitable et divertissante. Nous mettons constamment à jour notre plateforme pour intégrer de nouveaux jeux et améliorer votre expérience de jeu.
+
+Prêt à relever de nouveaux défis et à découvrir l'excitation des jeux de plateau en ligne ? Inscrivez-vous dès maintenant sur PlateauVirtuoso et laissez l'aventure commencer !</p>
+                   
+                    
                 </div>
-                <div class="col-lg-3 col-md-6 footer-links">
-                <h4>Useful links</h4>
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="index.php">A propos</a></li>
-                        <li><a href="index.php">Contact</a></li>
-                        <li><a href="index.php">Services</a></li>
-                        <li><a href="index.php">Privacy policy</a></li>
-                    </ul>
-                </div>
+                
                     
                 
-                <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Our Services</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus beatae commodi, dicta quos ab vero deserunt nostrum modi nesciunt doloribus magnam dolor temporibus voluptatum iure velit praesentium illo amet fuga.</p>
-                    <ul>
-                        <li><a href="#">Smart Phone</a></li>
-                        <li><a href="#">Smart watch</a></li>
-                        <li><a href="#">Airpods</a></li>
-                    </ul>
+                <div class="col-lg-4 col-md-6 footer-links">
+                    <h3>Nos Services</h3>
+                    <p>Bien plus qu'une simple plateforme de jeux de plateau en ligne, PlateauVirtuoso vous offre une gamme de services exceptionnels pour enrichir votre expérience de jeu. Découvrez comment nous allons au-delà des attentes pour rendre votre aventure ludique encore plus passionnante.</p>
+                   
                 </div>
-                <div class="col-lg-3 col-md-6 footer-link">
-                    <h4>Our Social Network</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sunt quibusdam culpa, itaque consectetur dolorum. Natus dolores maiores inventore laborum recusandae consectetur sunt impedit aut. Voluptas iste officia at incidunt?</p>
+                <div class="col-lg-4 col-md-6 footer-link">
+                    <h3>Contact</h3>
+                    <strong><i class="fas fa-phone"></i>Phone: <strong>+33 7 65 68 83 85</strong></strong><br>
+                    <strong><i class="fas-solid fa-envelope"></i>Email: <strong>cadhel.agossa@groupe-esigelec.org</strong></strong>
                     <div class="socail-links mt-3">
-                        <a href="#" class="twitter"><i class="fa-brands fa-twitter"></i></a>
-                        <a href="#" class="twitter"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" class="twitter"><i class="fa-brands fa-google-plus"></i></a>
-                        <a href="#" class="twitter"><i class="fa-brands fa-instagram"></i></a>
-                        <a href="#" class="twitter"><i class="fa-brands fa-linkedin-in"></i></a>
+                       
+                        <a href="https://www.linkedin.com/in/charly-ken-capo-chichi-05ab99268/" class="linkedin"><i class="fa-brands fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
@@ -158,7 +152,7 @@ session_start();
     <hr>
     <div class="container py-4">
         <div class="copyright">
-            &copy; Copyright<strong>Fashion</strong>.All Rights Reserved
+            &copy; Copyright<strong>Games</strong>.All Rights Reserved
         </div>
     </div>
     </footer>
